61b3a3dcc34c0e7a18bc3e5599bda55fcb4c809c
tglide <26071571+TGlide@users.noreply.github.com>
2023-05-19 08:59:54 +0100

    refactor: design changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,44 @@
 <script lang="ts">
-	import { Dialog } from 'radix-svelte';
+	import { Avatar, Dialog } from 'radix-svelte';
+	import { Calendar } from 'lucide-svelte';
 
 	let open = false;
 </script>
 
 <div class="mx-auto mt-4 flex max-w-lg flex-wrap gap-8">
 	{#each { length: 20 } as _}
-		<button on:click={() => (open = true)} class="w-full">
-			<img
-				src="http://placekitten.com/g/512/512"
-				alt="cute kitten"
-				class="w-full rounded-lg object-cover"
-			/>
-		</button>
+		<div>
+			<div class="flex items-center justify-between">
+				<div class="flex items-center">
+					<Avatar.Root
+						class="h-10 w-10 select-none items-center justify-center overflow-hidden rounded-full"
+					>
+						<Avatar.Image
+							class="h-full w-full rounded-[inherit] object-cover"
+							src="https://avatars.githubusercontent.com/u/26071571"
+							alt="Thomas G. Lopes Avatar"
+						/>
+						<Avatar.Fallback
+							class="text-md flex h-full w-full items-center justify-center bg-amaranth-300 font-medium text-white"
+						>
+							TL
+						</Avatar.Fallback>
+					</Avatar.Root>
+					<span class="ml-2 font-semibold text-amaranth-600">Thomas G. Lopes</span>
+				</div>
+				<div class="flex items-center gap-2 text-zinc-400">
+					<span class="text-sm ">2 days ago</span>
+					<Calendar class="h-5 w-5" />
+				</div>
+			</div>
+			<button on:click={() => (open = true)} class="mt-2 w-full">
+				<img
+					src="http://placekitten.com/g/512/512"
+					alt="cute kitten"
+					class="w-full rounded-lg object-cover"
+				/>
+			</button>
+		</div>
 	{/each}
 </div>
 
